b74a6c0fed7be72bf1a75a4755fe69bbf452829c
Joliver Estampador <joliver.estampador@tooltwist.com>
2020-07-08 15:28:19 +0800

    Supported Annotations

--- src/main/java/com/demo/json/CustomAnnotator.java
+++ src/main/java/com/demo/json/CustomAnnotator.java
@@ -2,20 +2,19 @@ package com.demo.json;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.sun.codemodel.JDefinedClass;
-import com.sun.codemodel.JFieldVar;
+import lombok.Builder;
+import lombok.Data;
 import org.jsonschema2pojo.AbstractAnnotator;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
 public class CustomAnnotator extends AbstractAnnotator {
+
     @Override
-    public void propertyField(JFieldVar field, JDefinedClass clazz, String propertyName, JsonNode propertyNode) {
-        super.propertyField(field, clazz, propertyName, propertyNode);
-
-        // Note: does not have to be the propertyName, could be the field or propertyNode that is verified.
-        if (propertyName.equals("entity")) {
-            field.annotate(Override.class);
-        }
-//        System.out.println("YEAH");
+    public void propertyOrder(JDefinedClass clazz, JsonNode propertiesNode) {
+        super.propertyOrder(clazz, propertiesNode);
+        clazz.annotate(Data.class);
+        clazz.annotate(Builder.class);
+        clazz.annotate(JsonAutoDetect.class);
+        System.out.println("test");
     }
-
-
 }
